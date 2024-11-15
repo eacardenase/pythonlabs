091d96564e980c2f37d1734f3359e1c1322aa08d
Edwin Cardenas <eacardenase@gmail.com>
2024-11-15 23:22:18 +0000

    feat: add is_palindrome for fun

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 def reverse(text: str) -> str:
     return text if text == "" else text [-1] + reverse(text[:-1])
 
+def is_palindrome(text: str) -> bool:
+    if len(text) <= 1:
+        return True
+
+    if text[0] != text[-1]:
+        return False
+
+    return is_palindrome(text[1:-1])
+
 def main():
-    print(reverse("straw"))
-    print(reverse("hello"))
-    print(reverse("world"))
+    # print(reverse("straw"))
+    # print(reverse("hello"))
+    # print(reverse("world"))
+    print(is_palindrome("world"))
+    print(is_palindrome("seres"))
+    print(is_palindrome("reviver"))
+    print(is_palindrome("coke"))
+    print(is_palindrome("racecar"))
 
 if __name__ == "__main__":
     main()
